aea7d6e79e5beb6eda20ace5fe459cafa07bd71b
Mike Mulev <m.mulev@gmail.com>
2026-05-04 22:56:41 +0200

    fix(tests): linter warning

--- flureadium/example/integration_test/epub_test.dart
+++ flureadium/example/integration_test/epub_test.dart
@@ -1,5 +1,4 @@
 import 'package:flureadium/flureadium.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
